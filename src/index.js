3757568a34f5ea3fff8319fa45f52233450b0585
nossulenko <nossulenkonikolai@gmail.com>
2025-08-29 20:47:30 +0200

    Fix build: remove import reference to deleted global.css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { Main } from './main.js';
-import './pages/styles/global.css';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
